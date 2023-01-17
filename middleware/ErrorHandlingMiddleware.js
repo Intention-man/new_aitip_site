c8c7a90a76465863192a50709abc07f92de9f228
DoubleZ <74947150+MishailAJ@users.noreply.github.com>
2023-01-17 16:17:37 +0300

    Delete middleware directory

--- middleware/ErrorHandlingMiddleware.js
+++
@@ -1,8 +0,0 @@
-const ApiError = require("../error/ApiError")
-
-module.exports = function (err, req, res, next) {
-    if (err instanceof ApiError) {
-        return res.status(err.status).json({message: err.message})
-    }
-    return res.status(500).json({message: "Непредвиденная ошибка"})
-}

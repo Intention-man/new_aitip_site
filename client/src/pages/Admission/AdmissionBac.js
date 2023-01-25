4259b08d1402264b1d86aff61d894c685f3a996a
amphyxs <amphyxs@gmail.com>
2023-01-25 15:06:55 +0300

    Moved AdmissionBac contents' declaration to return instead doing this inside contentBlocksData state

--- client/src/pages/Admission/AdmissionBac.js
+++ client/src/pages/Admission/AdmissionBac.js
@@ -16,14 +16,19 @@ const AdmissionBac = () => {
     const {admission_store} = useContext(Context);
     const setContent = useContext(ContentContext);  // Получаем callback из ContentContext для передачи текущих активных блоков
     
-    // Текущие блоки страницы
-    const [contentCards, setContentCards] = useState([
-        {name: 'Поступление в АИТИП', component: <AdmissionBacContent1 handleRef={element => setDomNode(element, 0)}/>},
-        {name: 'Выбрать направление', component: <AdmissionBacContent2 handleRef={element => setDomNode(element, 1)}/>},
-        {name: 'Траектория поступления', component: <AdmissionBacContent3 handleRef={element => setDomNode(element, 2)}/>},
-        {name: 'Важные даты', component: <AdmissionBacContent4 handleRef={element => setDomNode(element, 3)}/>},
-        {name: 'Про оплату обучения', component: <AdmissionBacContent5 handleRef={element => setDomNode(element, 4)}/>},
-        {name: 'Важные документы', component: <AdmissionBacContent6 handleRef={element => setDomNode(element, 5)}/>},
+    /* 
+        Этот state будет содержать следующие данные о блоках страницы:
+        name - подпись текущего блока в панели с ссылками
+        domNode - DOM-нода блока, чтобы была возможность ссылаться на него
+        index - порядковый номер
+    */
+    const [contentBlocksData, setContentBlocksData] = useState([
+        {name: 'Поступление в АИТИП'},
+        {name: 'Выбрать направление'},
+        {name: 'Траектория поступления'},
+        {name: 'Важные даты'},
+        {name: 'Про оплату обучения'},
+        {name: 'Важные документы'},
     ]);
 
     useEffect(() => {
@@ -32,27 +37,33 @@ const AdmissionBac = () => {
             console.log(1);
             console.log(admission_store.directions_bachelor);
         });
-        setContent(contentCards); // Вызываем callback 
+        setContent(contentBlocksData); // Вызываем callback 
     }, []);
 
     /**
-     * Это callback для того, чтобы обработчик React ref передал от каждого компонента из contentCards свою DOM-ноду и она была записана
+     * Это callback для того, чтобы обработчик React ref передал от каждого компонента из contentBlocksData свою DOM-ноду и она была записана
      * TODO: возможно это костыль
      * @param {React.Component} element 
      * @param {Number} index 
      */
     const setDomNode = (element, index) => {
-        const updatedContentCards = [...contentCards];  // Копируем текущее значение contentCards
-        updatedContentCards[index].domNode = findDOMNode(element);  // Сохраняем DOM-ноду компонента по переданному индексу
-        updatedContentCards[index].id = index;  // Сохраняем индекс компонента (просто его порядковый номер)
-        setContentCards(updatedContentCards);  // Обновляем contentCards
+        if (contentBlocksData[index].domNode)
+            return;
+        const updatedContentBlocksData = [...contentBlocksData];  // Копируем текущее значение contentBlocksData
+        updatedContentBlocksData[index].domNode = findDOMNode(element);  // Сохраняем DOM-ноду компонента по переданному индексу
+        updatedContentBlocksData[index].id = index;  // Сохраняем индекс компонента (просто его порядковый номер)
+        setContentBlocksData(updatedContentBlocksData);  // Обновляем contentBlocksData
     };
     
     return (
         <CardContainer>
-            {
-                contentCards.map(x => x.component)
-            }
+            {/* Проп handeRef передаётся, чтобы обработчик React ref вызвал callback setDomNode и был записана DOM-нода каждого компонента */}
+            <AdmissionBacContent1 handleRef={element => setDomNode(element, 0)}/> 
+            <AdmissionBacContent2 handleRef={element => setDomNode(element, 1)}/>
+            <AdmissionBacContent3 handleRef={element => setDomNode(element, 2)}/>
+            <AdmissionBacContent4 handleRef={element => setDomNode(element, 3)}/>
+            <AdmissionBacContent5 handleRef={element => setDomNode(element, 4)}/>
+            <AdmissionBacContent6 handleRef={element => setDomNode(element, 5)}/>
         </CardContainer>
     );
 }

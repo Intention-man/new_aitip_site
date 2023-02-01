0a3352b05b7c8d5f690fe775d358f3d30d01ec7c
sergeyprohaker <42sdn@mail.ru>
2023-02-01 11:42:45 +0300

    Added history of university

---
+++ client/src/components/card/CardContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../../css/component_styles/Card.css';
+
+class CardContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="CardContainer content">
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default CardContainer;

c9cb6295ce6fb087348f278b718a1f3c6ed80fa3
amphyxs <amphyxs@gmail.com>
2023-02-05 15:42:46 +0300

    Basic functionality for Block & BlockContainer components

--- client/src/components/card/CardContainer.js
+++ client/src/components/card/CardContainer.js
@@ -1,6 +1,11 @@
 import React from "react";
 import '../../css/component_styles/Card.css';
 
+/**
+ * @deprecated
+ * Компонент больше не используется, однако он оставлен для того,
+ * чтобы избежать ошибок.
+ */
 class CardContainer extends React.Component {
     constructor(props) {
         super(props);
---
+++ client/src/components/card/StyledText.js
@@ -0,0 +1,12 @@
+import '../../css/component_styles/Block.css';
+
+
+const StyledText = ({ children }) => {
+    return (
+        <div className="StyledText">
+            { children }
+        </div>
+    );
+}
+
+export default StyledText;

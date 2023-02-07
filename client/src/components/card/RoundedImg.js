fb562d0fedd2f7f10cfdbc6517341bc69c23749f
amphyxs <amphyxs@gmail.com>
2023-02-07 23:45:41 +0300

    Added docs for new components

--- client/src/components/card/RoundedImg.js
+++ client/src/components/card/RoundedImg.js
@@ -7,10 +7,8 @@ import '../../css/component_styles/Card.css'
  * ```
     <RoundedImg
         imgSrc='path/to/img'
-        size={100}
     />
  * ```
- * Данный код задаёт круглую картинку с диаметром, равным 100 пикселям.
  * 
  * Props:
  * @param {Object | string} imgSrc - изображение (либо импортированное, либо путь к нему).
--- client/src/components/card/StyledText.js
+++ client/src/components/card/StyledText.js
@@ -1,6 +1,13 @@
 import '../../css/component_styles/Block.css';
 
-
+/**
+ * Линия текстового контента. Она нужна для того, чтобы применять необходимые стили для текста.
+ * 
+ * Допустимые дочерние теги:
+ * - `<p>`
+ * - `<h1>`,`<h2>`,`<h3>`
+ * - `<ul>`, `<ol>`, `<li>`
+ */
 const StyledText = ({ children }) => {
     return (
         <div className="StyledText">

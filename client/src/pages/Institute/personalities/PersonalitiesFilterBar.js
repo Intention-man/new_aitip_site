9f95efa68854930a4400c0bc5a5f06811467dea0
sergeyprohaker <42sdn@mail.ru>
2023-02-08 15:15:17 +0300

    Updated filters

--- client/src/pages/Institute/personalities/PersonalitiesFilterBar.js
+++ client/src/pages/Institute/personalities/PersonalitiesFilterBar.js
@@ -3,46 +3,65 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {observer} from "mobx-react-lite";
 import {Context} from "../../../index";
+import "../../../css/component_styles/Personalities.css"
+
 
 
 const PersonalitiesFilterBar = observer(({filteredDirections, setFilteredDirections, filteredPrograms, setFilteredPrograms}) => {
     const {admission_store} = useContext(Context)
 
     return (
-        <div style={{display: "block", margin: "0 1%", justifyContent: "left"}}>
+        <div style={{display: "block", margin: "0 1%", justifyContent: "left", background: "white"}}>
+            <div style={{background: "white", padding: "10px"}}>
+                <p className="filter_name">По направлениям:</p>
 
-            <p>По направлениям бакалавриата</p>
 
-            {(admission_store.directionsBachelor && admission_store.directionsBachelor.length !== 0) && admission_store.directionsBachelor.map(direction =>
-                <button
-                    style={{cursor: "pointer", borderColor: "lightgray"}}
-                    key={direction.name}
-                    onClick={() => {
-                        filteredDirections.includes(direction)
-                            ? setFilteredDirections(filteredDirections => filteredDirections.filter(i => i !== direction))
-                            : setFilteredDirections(filteredDirections => [...filteredDirections, direction])
-                    }
-                    }
-                >
-                    {direction.name}
+                {/*{(admission_store.directionsBachelor && admission_store.directionsBachelor.length !== 0) && admission_store.directionsBachelor.map(direction =>*/}
+                {/*    <button className="filter_button"*/}
+                {/*            key={direction.name}*/}
+                {/*            onClick={() => {*/}
+                {/*                filteredDirections.includes(direction)*/}
+                {/*                    ? setFilteredDirections(filteredDirections => filteredDirections.filter(i => i !== direction))*/}
+                {/*                    : setFilteredDirections(filteredDirections => [...filteredDirections, direction])*/}
+                {/*            }*/}
+                {/*            }*/}
+                {/*    >*/}
+                {/*        {direction.name}*/}
+                {/*    </button>*/}
+                {/*    <div className="squaredOne">*/}
+                {/*        <input type="checkbox" value="None" id="squaredOne"/>*/}
+                {/*        <label htmlFor="squaredOne"></label>*/}
+                {/*    </div>*/}
+                <div className="squaredOne">
+                    <input type="checkbox" id="squaredOne" style={{zIndex: "1000"}}/>
+                    <label htmlFor="squaredOne"></label>
+
+                    {/*<label><input type="checkbox"/>Менеджмент</label><br/>*/}
+                    {/*<label><input type="checkbox"/>Экономика</label><br/>*/}
+                </div>
+
+                {/*)}*/}
+
+                <p className="filter_name">По программам ДПО:</p>
+
+                {(admission_store.additionalPrograms && admission_store.additionalPrograms.length !== 0) && admission_store.additionalPrograms.map(program =>
+                    <button className="filter_button"
+                            key={program.name}
+                            onClick={() => {
+                                filteredPrograms.includes(program)
+                                    ? setFilteredPrograms(filteredPrograms => filteredPrograms.filter(i => i !== program))
+                                    : setFilteredPrograms(filteredPrograms => [...filteredPrograms, program])
+                            }}
+                    >
+                        {program.name}
+                    </button>
+                )}
+
+                <button className="undo_filters">
+                    Сбросить фильтры
                 </button>
-            )}
+            </div>
 
-            <p>По программам ДПО</p>
-
-            {(admission_store.additionalPrograms && admission_store.additionalPrograms.length !== 0) && admission_store.additionalPrograms.map(program =>
-                <button
-                    style={{cursor: "pointer", borderColor: "lightgray"}}
-                    key={program.name}
-                    onClick={() => {
-                        filteredPrograms.includes(program)
-                            ? setFilteredPrograms(filteredPrograms => filteredPrograms.filter(i => i !== program))
-                            : setFilteredPrograms(filteredPrograms => [...filteredPrograms, program])
-                    }}
-                >
-                    {program.name}
-                </button>
-            )}
         </div>
     );
 })

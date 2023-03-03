91311dfb0b80662a17985bf97594b6b8b015c9fd
sergeyprohaker <42sdn@mail.ru>
2023-03-03 16:26:58 +0300

    Changes by Михаил

--- client/src/pages/Institute/personalities/PersonalitiesList.js
+++ client/src/pages/Institute/personalities/PersonalitiesList.js
@@ -1,12 +1,13 @@
 // Отрисовка всех сотрудников на странице Персоналии
 
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 
 import {Context} from "../../../index";
 import StafferItem from "./StafferItem";
 import {observer} from "mobx-react-lite";
 import "../../../css/page_styles/PersonalityList.css"
 import {Row} from "react-bootstrap";
+import RoundedImg from "../../../components/lines/RoundedImg";
 
 
 const PersonalitiesList = observer(params => {
@@ -37,17 +38,16 @@ const PersonalitiesList = observer(params => {
                     for (let i = 0; i < linesCount; i += groupSize) {
                         let lastSeveralStaffId = []
                         const staffer1 = (params.filteredStaff.length > i ? staff_store.staff[i] : undefined)
-                        const staffer2 = (params.filteredStaff.length > (i + 1) ? staff_store.staff[i + 1] : undefined)
-                        // const staffer3 = (params.filteredStaff.length > (i + 2) ? staff_store.staff[i + 2] : undefined)
-                        if (staffer1 !== undefined) {lastThreeStaffId.push(staffer1.id)}
-                        if (staffer2 !== undefined) lastThreeStaffId.push(staffer2.id)
-                        // if (staffer3 !== undefined) lastThreeStaffId.push(staffer3.id)
+                        const staffer2 = groupSize > 1 ? (params.filteredStaff.length > (i + 1) ? staff_store.staff[i + 1] : undefined) : undefined
+                        const staffer3 = groupSize > 2 ? (params.filteredStaff.length > (i + 2) ? staff_store.staff[i + 2] : undefined) : undefined
+                        if (staffer1 !== undefined) {lastSeveralStaffId.push(staffer1.id)}
+                        if (staffer2 !== undefined) lastSeveralStaffId.push(staffer2.id)
+                        if (staffer3 !== undefined) lastSeveralStaffId.push(staffer3.id)
 
-                        let list = [staffer1, staffer2
-                           // , staffer3
-                        ].filter(i => i !== undefined)
+                        let list = [staffer1, staffer2, staffer3].filter(i => i !== undefined)
 
-                        rows.push(<Row style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr"}}>
+                        rows.push(<Row style={{display: "grid", gridTemplateColumns:
+                                (groupSize === 1 ? "1fr" : (groupSize === 2 ? "1fr 1fr" : "1fr 1fr 1fr"))}}>
                             {list.map(staffer =>
                                 <div key={staffer.id}
                                      className="person_block"
@@ -56,7 +56,7 @@ const PersonalitiesList = observer(params => {
                                      }
                                      }>
                                         <div className="block_content">
-                                    <img src={process.env.REACT_APP_API_URL + staffer.img}
+                                    <RoundedImg imgSrc={process.env.REACT_APP_API_URL + staffer.img}
                                          className="ava_img"
                                          alt="картинка чет не загрузилась"/>
                                     <div>
@@ -67,7 +67,7 @@ const PersonalitiesList = observer(params => {
                                 </div>
                             )}
                         </Row>)
-                        rows.push(params.chosenStaffer && lastThreeStaffId.includes(params.chosenStaffer.id) &&
+                        rows.push(params.chosenStaffer && lastSeveralStaffId.includes(params.chosenStaffer.id) &&
                             <StafferItem key={params.chosenStaffer.id} staffer={params.chosenStaffer}/>)
                     }
                     return rows

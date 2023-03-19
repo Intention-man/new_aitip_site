dfd55f460b0d3c8ee733eada2b4d4b7fd14c516b
RAZRULETEL <qwerty.qwertlain@gmail.com>
2023-03-19 22:58:24 +0300

    Кнопка, список кнопок и стили к ним

---
+++ client/src/components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../css/page_styles/AdminPanel.css"
+import {observer} from "mobx-react-lite";
+import "../css/component_styles/Button.css";
+
+
+
+// принимает текст надписи, функцию устанвливаемого значения и устанвливаемое значение(опционально)
+const Button = observer(({buttonName, valueToSet, setChosenValue}) => {
+    return (
+        <button className="custom_button" key={valueToSet ? valueToSet : buttonName} onClick={() => setChosenValue(valueToSet ? valueToSet : buttonName)}>
+            {buttonName}
+        </button>
+    );
+});
+
+export default Button;

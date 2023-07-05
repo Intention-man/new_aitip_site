a1b93d61c0b6b4ac26e72db35b54b3517cbedcfe
DoubleZ <mihailagei2019@mail.ru>
2023-07-05 20:10:26 +0700

    vercel trying

--- server/db.js
+++ server/db.js
@@ -11,22 +11,56 @@ const pool = new Pool({
 module.exports = pool
 
 const {Sequelize} = require("sequelize");
-// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-// module.exports = new Sequelize({
-//     database: PGDATABASE,
-//     username: PGUSER,
-//     password: PGPASSWORD,
-//     host: PGHOST,
-//     port: 5432,
-//     dialect: "postgres",
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     },
-// });
+module.exports = new Sequelize(
+    process.env.POSTGRES_DATABASE,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    {
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        host: process.env.POSTGRES_HOST,
+        port: 5432,
+        password: process.env.POSTGRES_PASSWORD
+    }
+);
+
+// import pg from 'pg';
+
+// const { Pool } = pg;
+//
+// const pool = new Pool({
+//     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// })
+
+// module.exports = pool
+
+
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: "postgres",
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         password: process.env.DB_TABLE_PASSWORD
+//     }
+// )
+
+
+// dialectOptions: {
+//     ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//     }
+// },
+
 // module.exports = new Sequelize
 // ('postgres://MishailAJ:ydpO3ZcEx7en@eu-central-1.aws.neon.tech/neondb?sslmode=no-verify')
 

bb50899c8daf48638dcaaf96bb027ea16bf4ec3d
DoubleZ <mihailagei2019@mail.ru>
2023-07-11 10:26:38 +0700

    Small data changed

--- server/db.js
+++ server/db.js
@@ -2,15 +2,35 @@
 
 const {Sequelize} = require("sequelize");
 
+// Для подключения к локальной БД
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_TABLE_PASSWORD,
+//     {
+//         dialect: "postgres",
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         password: process.env.DB_TABLE_PASSWORD
+//     }
+// )
+
+// Для подключения к БД на neon
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.PGDATABASE,
+    process.env.PGUSER,
+    process.env.PGPASSWORD,
     {
         dialect: "postgres",
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        password: process.env.DB_TABLE_PASSWORD
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        host: process.env.PGHOST,
+        port: 5432,
+        password: process.env.PGPASSWORD
     }
-)
+);
--- server/index.js
+++ server/index.js
@@ -42,22 +42,3 @@ const start = async () => {
 
 
 start()
-
-// Do not expose your Neon credentials to the browser
-// .env
-
-
-// app.js
-// const postgres = require('postgres');
-//
-// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
-//
-// const sql = postgres(URL, { ssl: 'require' });
-//
-// async function getPgVersion() {
-//     const result = await sql`select version()`;
-//     console.log(result);
-// }
-//
-// getPgVersion();

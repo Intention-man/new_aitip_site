cd3d17414e246e7b6a16320e2c08eece8ff49b1e
RAZRULETEL <qwerty.qwertlain@gmail.com>
2023-09-16 12:22:53 +0300

    Небольшое исправление стилей ButtonList

--- client/src/components/ButtonList.js
+++ client/src/components/ButtonList.js
@@ -9,7 +9,7 @@ const ButtonList = observer(({className, buttonList, setChosenValue, isStretchLa
     return (
         <ul className={"button_list "+(className !== undefined ? className : "")}>
             {(Array.isArray(buttonList)?buttonList:Object.keys(buttonList)).map((key, index, arr) =>
-                <button className={"button-admin" + (arr.length-1 === index) && arr.length % 2 === 1 ? "stretch" : ""} key={key} onClick={() => setChosenValue(key)}>
+                <button className={"button-admin" + ((arr.length-1 === index) && arr.length % 2 === 1 ? " stretch" : "")} key={key} onClick={() => setChosenValue(key)}>
                     {key}
                 </button>
             )}
